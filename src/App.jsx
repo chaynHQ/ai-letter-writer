fc97035ca2e9ebbdb64a6085babff5b1a47c6696
yublee71 <lybin71@naver.com>
2024-11-22 12:27:09 +0000

    feat: convert vite to react

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import { Route, Routes } from "react-router-dom";
 
-import { Disclaimer, TellStory } from './pages'
-import './App.css'
+import { Disclaimer, TellStory } from "./pages";
+import "./App.css";
+// import logo from "./logo.svg";
 
 function App() {
- 
   return (
     <Routes>
       <Route path="/" element={<Disclaimer />} />
       <Route path="/TellStory" element={<TellStory />} />
     </Routes>
-  )
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
 }
 
-export default App
+export default App;
